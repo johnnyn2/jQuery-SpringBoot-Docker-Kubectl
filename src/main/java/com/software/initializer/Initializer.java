b2e50f164a9ee29e97db06ae28cddffd53fb0800
Johnny Ho Wai Kin <johnnyho@hket.com>
2020-06-03 16:26:04 +0800

    put starter to initializer

--- src/main/java/com/software/initializer/Initializer.java
+++ src/main/java/com/software/initializer/Initializer.java
@@ -1,5 +1,18 @@
 package com.software.initializer;
 
-public class Initializer {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@SpringBootApplication(scanBasePackages = { "com.software.*" })
+public class Initializer extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Initializer.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Initializer.class, args);
+	}
 }
