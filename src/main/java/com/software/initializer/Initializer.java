fec384bee72333922e268001a0ef9e53caf9d735
Johnny Ho Wai Kin <johnnyho@hket.com>
2020-06-05 10:09:53 +0800

    fixed bug

--- src/main/java/com/software/initializer/Initializer.java
+++ src/main/java/com/software/initializer/Initializer.java
@@ -1,12 +1,19 @@
 package com.software.initializer;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = { "com.software.*" })
+@SpringBootApplication
+@EnableAutoConfiguration()
+@EnableJpaRepositories(basePackages = { "com.software.repository" })
+@ComponentScan(value = { "com.software.*" })
+@EntityScan("com.software.*")
 public class Initializer extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
