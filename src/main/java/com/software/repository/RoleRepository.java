34596c7481fa85b7ce7d080d89f6602df51706d9
wai kin ho <wkhoae@connect.ust.hk>
2020-06-07 17:36:33 +0800

    add role repo

--- src/main/java/com/software/repository/RoleRepository.java
+++ src/main/java/com/software/repository/RoleRepository.java
@@ -1,5 +1,30 @@
 package com.software.repository;
 
+import javax.transaction.Transactional;
+
+import com.software.model.Role;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Transactional
 public class RoleRepository {
+    @Autowired
+    private SessionFactory factory;
     
+    public Role save(Role role) {
+        getSession().save(role);
+        return role;
+    }
+
+    private Session getSession() {
+		Session session = factory.getCurrentSession();
+		if (session == null) {
+			session = factory.openSession();
+		}
+		return session;
+	}
 }
