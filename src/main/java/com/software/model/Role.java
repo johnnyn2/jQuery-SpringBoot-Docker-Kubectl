83ec32dc655b5aa5c45f4edd7202354d914f83f8
Johnny Ho Wai Kin <johnnyho@hket.com>
2020-06-08 11:12:51 +0800

    generated value

--- src/main/java/com/software/model/Role.java
+++ src/main/java/com/software/model/Role.java
@@ -2,6 +2,7 @@ package com.software.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -15,6 +16,7 @@ import lombok.Setter;
 public class Role {
     @Id
     @Column
+    @GeneratedValue
     private int role_id;
     @Column
     private String name;
